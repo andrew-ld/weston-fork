ef377bbb11254e6612c5938488fd376317565f0a
andrew-ld <43882924+andrew-ld@users.noreply.github.com>
2025-08-03 20:04:23 +0200

    use dma feedback formats in egl

--- libweston/backend-wayland/wayland.c
+++ libweston/backend-wayland/wayland.c
@@ -811,10 +811,9 @@ get_host_formats(struct weston_compositor *compositor, unsigned *count_out,
 
   if (count > 0) {
     info_array = pixel_format_get_array(formats_list, count);
-    if (!info_array) {
-      return NULL;
+    if (info_array) {
+      *count_out = count;
     }
-    *count_out = count;
   }
 
   free(formats_list);
