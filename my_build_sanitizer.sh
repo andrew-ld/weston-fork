611b05f9bc4e49d4bdf02522be04045eae08daf1
andrew-ld <43882924+andrew-ld@users.noreply.github.com>
2025-08-05 20:41:10 +0200

    fix memory leaks

---
+++ my_build_sanitizer.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+rm -rf build || true
+
+meson build/ \
+    -Db_sanitize=address,undefined \
+    -Dbackend-wayland=true \
+    -Dbackend-x11=false \
+    -Drenderer-gl=true \
+    -Dshell-desktop=false \
+    -Dxwayland=true \
+    -Dbackend-drm=false \
+    -Dbackend-headless=false \
+    -Dbackend-pipewire=false \
+    -Dbackend-rdp=false \
+    -Dbackend-vnc=false \
+    -Drenderer-vulkan=false \
+    -Dremoting=false \
+    -Dpipewire=false \
+    -Dshell-ivi=false \
+    -Dshell-kiosk=true \
+    -Dshell-lua=false \
+    -Dcolor-management-lcms=false \
+    -Dimage-jpeg=false \
+    -Dimage-webp=false \
+    -Ddemo-clients=false \
+    -Dtools=[] \
+    -Dtests=false \
+    -Ddoc=false \
+    -Dperfetto=false \
+    -Dbackend-default=wayland \
+    -Ddefault_library=static \
+    -Ddefault_both_libraries=static \
+    -Dsystemd=false \
+    -Dtest-junit-xml=false \
+    --prefix=$(pwd)/install \
+    -Ddesktop-shell-client-default=kiosk \
+    -Dsimple-clients=[]
+
+ninja -C build/ install
